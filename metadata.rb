40f2b3fc5ef9aad610760ad6f6529fc6b1a0bc7e
Peter de Ruijter <peter@l2code.nl>
2013-08-06 09:25:45 +0200

    Sunrise commit. Adds the recipe and the necessary metadata files.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "pgbadget-chef"
+maintainer       "Peter de Ruijter"
+maintainer_email "peter@springest.com"
+license          "MIT"
+description      "Installs pgbadget, log analysis and report generation for postgres"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.0.1"
+
+depends "perl"
---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+git "/data/springest/pgbadger" do
+  repository "https://github.com/dalibo/pgbadger.git"
+  reference "a614611252c46d13f2f0cb03040c21c0b6b9a692"
+  action :sync
+  notifies :run, 'execute[run Makefile]', :immediately
+  not_if do ::File.exists?("/usr/bin/pgbadger") end
+end
+
+execute "run Makefile" do
+	cwd "/data/springest/pgbadger"
+	command "perl Makefile.PL INSTALLDIRS='perl'"
+	action :nothing
+	notifies :run, 'execute[make and install]', :immediately
+end
+
+execute "make and install" do
+	cwd "/data/springest/pgbadger"
+	command "make & make install"
+	action :nothing
+end
