5ec4caaf0666751ae2d6ad3d2c9ebad529e3f3de
Wouter de Vos <wouter@springest.com>
2014-08-28 15:17:10 +0200

    Rename to pgbadger.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             "pgbadget-chef"
+name             "pgbadger"
 maintainer       "Peter de Ruijter"
 maintainer_email "peter@springest.com"
 license          "MIT"
-description      "Installs pgbadget, log analysis and report generation for postgres"
+description      "Installs pgbadger, log analysis and report generation for postgres"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.0.1"
 
